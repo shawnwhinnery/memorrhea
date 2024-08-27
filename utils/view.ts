0b489ffc0a1f3740179ea3709a78927edfd7c14c
shawnwhinnery <shawnsdrive@gmail.com>
2024-08-26 17:17:17 -0700

    foo

--- utils/view.ts
+++ utils/view.ts
@@ -1,9 +1,28 @@
-import { allocate, lock, Pointer, unlock, watch, write, deref } from './memory'
+import { allocate, lock, Pointer, unlock, watch, write, deref } from '../memory'
 
 /**
  * @function view
  * @description A view allocates a dependant variable that is recomputed from multiple dependant variables.
  * @returns a pointer for the data view
+ * @example
+ * 
+ * import { view } from "memorrhea"
+ * import { auth } from "./state.ts"
+ * 
+ * const loginFormDisabled = view((email, password, loading) => {
+ *  return !email || !password || loading
+ * }, auth.email, auth.password, auth.loading)
+ * 
+ * 
+ * export const LoginForm () {
+ *  const [disabled] = usePointer(loginFormDisabled)
+ *  return <form>
+ *       <input />
+ *       <input />
+ *       <button type="submit" disabled={disabled}
+ *  <form>
+ * }
+ * 
  */
 export function view<T, U extends Pointer<any>[]>(
     reducer: (
@@ -42,3 +61,4 @@ export function view<T, U extends Pointer<any>[]>(
 
     return pointer
 }
+
